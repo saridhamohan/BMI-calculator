79be8f09c8d0bf3df0e8b8977863a8af284c2fa1
saridhamohan <85500063+saridhamohan@users.noreply.github.com>
2021-06-25 10:38:16 -0400

    Updates

---
+++ precache-manifest.3dff15a839b0986796c6960079e57349.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4f0e9a09f8150416e798ebe7832f0b45",
+    "url": "/BMI-calculator/index.html"
+  },
+  {
+    "revision": "95caad36a20e759385b6",
+    "url": "/BMI-calculator/static/css/2.ae9e8b99.chunk.css"
+  },
+  {
+    "revision": "e60ea1b520b3b17fe8fa",
+    "url": "/BMI-calculator/static/css/main.fce36487.chunk.css"
+  },
+  {
+    "revision": "95caad36a20e759385b6",
+    "url": "/BMI-calculator/static/js/2.4a2b1d12.chunk.js"
+  },
+  {
+    "revision": "e60ea1b520b3b17fe8fa",
+    "url": "/BMI-calculator/static/js/main.dae918e2.chunk.js"
+  },
+  {
+    "revision": "d5ccd8e97a8140fed806",
+    "url": "/BMI-calculator/static/js/runtime~main.912e8a5c.js"
+  }
+]);
